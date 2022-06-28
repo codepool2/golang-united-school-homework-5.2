0c862ff3eb433b125c53c3ed41d1d0a1addc805a
Gona Amarnath Reddy <agona@egencia.com>
2022-06-28 11:23:50 +0530

    init commit

--- cache.go
+++ cache.go
@@ -1,23 +1,82 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	data map[string]Value
+}
+
+type Value struct {
+	data      string
+	hasExpiry bool
+	validTime time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]Value)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache Cache) Get(key string) (string, bool) {
+
+	val, exist := cache.data[key]
+
+	if exist {
+
+		if isExpired(val) {
+			return "", false
+		} else {
+			return val.data, true
+		}
+
+	}
+
+	return "", exist
+}
+
+func getData(value Value) (string, bool) {
+
+	if isExpired(value) {
+		return "", true
+	}
+
+	return value.data, false
 
 }
 
-func (receiver) Put(key, value string) {
+func isExpired(value Value) bool {
+
+	return value.hasExpiry && time.Now().After(value.validTime)
+
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Put(key, value string) {
+
+	va := Value{
+		data:      value,
+		hasExpiry: false}
+	cache.data[key] = va
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) Keys() []string {
+
+	output := make([]string, len(cache.data))
+	for key, value := range cache.data {
+		if isExpired(value) {
+			delete(cache.data, key)
+		} else {
+			output = append(output, key)
+		}
+	}
+	return output
+}
+
+func (cache Cache) PutTill(key, value string, deadline time.Time) {
+
+	va := Value{
+		data:      value,
+		hasExpiry: true,
+		validTime: deadline}
+	cache.data[key] = va
 }
